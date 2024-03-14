621648f7c5cb789827e8b51ec9d5238b122ff983
davilos <daviloscostagg@hotmail.com>
2024-03-14 17:15:31 -0300

    feat: Adiciona a rota de POST para "transactions"

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -1,8 +1,28 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
+import { randomUUID } from "node:crypto"
 import { knex } from "../src/database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    return await knex('transactions').where('amount', 1000).select('*')
+  app.post('/', async (request, response) => {
+    // { title, amount, type: credit ou debit }
+    
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1
+    })
+
+    return response.status(201).send()
   })
 }
