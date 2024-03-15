508965dfc095c27c4aaff9d87d7deb6e89e01b92
davilos <daviloscostagg@hotmail.com>
2024-03-15 13:53:10 -0300

    feat: Adiciona três rotas para a tabela "transactions"

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -4,16 +4,41 @@ import { randomUUID } from "node:crypto"
 import { knex } from "../src/database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' }).first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, response) => {
     // { title, amount, type: credit ou debit }
     
-    const createTransactionBodySchema = z.object({
+    const bodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit'])
     })
 
-    const { title, amount, type } = createTransactionBodySchema.parse(
+    const { title, amount, type } = bodySchema.parse(
       request.body
     )
 
